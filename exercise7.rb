00cdfd8a485ac34c06c7e081c30e5d10b8bab3ea
Benjamin Alright <benjaminalright@Benjamins-MacBook-Air.local>
2015-01-21 19:46:53 -0500

    Increased each cohort by 5%

--- exercise7.rb
+++ exercise7.rb
@@ -13,5 +13,9 @@ display(students)
 students[:cohort4] = 43
 display(students)
 
-puts students.keys
+students.inject(students) {|students, (x, y)| students[x] = y * 1.05; students }
+
+display(students)
+
+
 
